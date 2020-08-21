0deb29849490d4a9abe41c10a3853b2adda01e3a
Nadir <nadiransarionline@gmail.com>
2020-08-21 03:01:36 -0700

    added block i

--- models/block.js
+++ models/block.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
  
 var BlockSchema = new Schema({
+  coin: { type: String },
   blockhash: { type: String, lowercase: true, unique: true, index: true},
   blockheight: { type: Number, default: 0, index: true },
   timestamp: { type: Number, default: 0, index: true },
